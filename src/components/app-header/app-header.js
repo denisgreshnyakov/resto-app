5956d168f9eccddc3ed20a890b2e57ac9e7eaaa2
Denis <denisgreshnaykov@gmail.com>
2022-10-01 13:16:27 +0500

    before homework

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import cartIcon from './shopping-cart-solid.svg';
-import './app-header.scss';
+import React from "react";
+import cartIcon from "./shopping-cart-solid.svg";
+import "./app-header.scss";
+import { Link } from "react-router-dom";
 
-const AppHeader = ({total}) => {
-    return (
-        <header className="header">
-            <a className="header__link" href="#">
-                Menu
-            </a>
-            <a className="header__link" href="#">
-                <img className="header__cart" src={cartIcon} alt="cart"></img>
-                Total: {total} $
-            </a>
-        </header>
-    )
+const AppHeader = ({ total }) => {
+  return (
+    <header className="header">
+      <a className="header__link" href="/">
+        Menu
+      </a>
+      <a className="header__link" href="#">
+        <img className="header__cart" src={cartIcon} alt="cart"></img>
+        Total: {total} $
+      </a>
+    </header>
+  );
 };
 
 export default AppHeader;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import {MainPage, CartPage} from '../pages';
-import AppHeader from '../app-header';
+import React from "react";
+import { MainPage, CartPage } from "../pages";
+import AppHeader from "../app-header";
+import WithRestoService from "../hoc";
+import { Route, Routes } from "react-router-dom";
 
-import Background from './food-bg.jpg';
+import Background from "./food-bg.jpg";
 
-const App = () => {
-    return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
-        </div>
-    )
-}
+const App = ({ RestoService }) => {
+  console.log(RestoService.getMenuItems());
+  return (
+    <div
+      style={{
+        background: `url(${Background}) center center/cover no-repeat`,
+      }}
+      className="app"
+    >
+      <AppHeader total={50} />
+      <Routes>
+        <Route path="/" element={<MainPage />} exact></Route>
+        <Route path="/cart" element={<CartPage />} exact></Route>
+      </Routes>
+    </div>
+  );
+};
 
-export default App;
+export default WithRestoService()(App);
